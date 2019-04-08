1a99b7cb8565cd0fae2a077dd8f27f6be100eb00
Joel Burget <joelburget@gmail.com>
2019-04-08 15:59:02 -0700

    Add variable evaluation.

--- src/Hutton.hs
+++ src/Hutton.hs
@@ -3,11 +3,13 @@ module Hutton where
 
 import Control.Monad.Reader
 import Data.Map (Map)
+import qualified Data.Map as Map
 import Data.SBV
 
 data Expr
   = Add Expr Expr
   | Lit Integer
+  | Var String
 
 type Eval = ReaderT (Map String (SBV Integer)) (Either String)
 
@@ -15,3 +17,8 @@ eval :: Expr -> Eval (SBV Integer)
 eval = \case
   Add x y -> (+) <$> eval x <*> eval y
   Lit i   -> pure $ literal i
+  Var v   -> do
+    env <- ask
+    case Map.lookup v env of
+      Nothing  -> lift $ Left $ "variable not found: " ++ v
+      Just val -> pure val
