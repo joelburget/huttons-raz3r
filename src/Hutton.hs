21ef12bb54ccb6bd65d9979c392033811bc7feb8
Joel Burget <joelburget@gmail.com>
2019-04-09 14:05:32 -0700

    Add conditionals.

--- src/Hutton.hs
+++ src/Hutton.hs
@@ -1,7 +1,9 @@
-{-# language GADTs      #-}
-{-# language LambdaCase #-}
+{-# language GADTs                      #-}
+{-# language GeneralizedNewtypeDeriving #-}
+{-# language LambdaCase                 #-}
 module Hutton where
 
+import Control.Monad.Error.Class
 import Control.Monad.Reader
 import Data.Map (Map)
 import qualified Data.Map as Map
@@ -9,18 +11,32 @@ import Data.SBV
 import qualified Data.SBV.Internals as SBVI
 
 data Expr ty where
-  Add :: Expr Integer -> Expr Integer -> Expr Integer
-  Lit :: SymVal a => a                -> Expr a
-  Var :: String                       -> Expr a
+  Add :: Expr Integer -> Expr Integer  -> Expr Integer
+  Ite :: Expr Bool -> Expr a -> Expr a -> Expr a
+  Lit :: SymVal a => a                 -> Expr a
+  Var :: String                        -> Expr a
 
-type Eval = ReaderT (Map String SBVI.SVal) (Either String)
+newtype Eval a = Eval { runEval :: ReaderT (Map String SBVI.SVal) (Either String) a }
+  deriving (Functor, Applicative, Monad, MonadReader (Map String SBVI.SVal),
+    MonadError String)
 
-eval :: Expr a -> Eval (SBV a)
+instance Mergeable a => Mergeable (Eval a) where
+  symbolicMerge force test left right = Eval $ ReaderT $ \env -> do
+    -- Note we're in the `Either String` monad and if either side fails the
+    -- whole computation does
+    left'  <- runReaderT (runEval left)  env
+    right' <- runReaderT (runEval right) env
+    pure $ symbolicMerge force test left' right'
+
+eval :: SymVal a => Expr a -> Eval (SBV a)
 eval = \case
   Add x y -> (+) <$> eval x <*> eval y
-  Lit i   -> pure $ literal i
-  Var v   -> do
+  Ite a b c -> do
+    a' <- eval a
+    ite a' (eval b) (eval c)
+  Lit i -> pure $ literal i
+  Var v -> do
     env <- ask
     case Map.lookup v env of
-      Nothing   -> lift $ Left $ "variable not found: " ++ v
+      Nothing   -> throwError $ "variable not found: " ++ v
       Just sval -> pure $ SBVI.SBV sval
